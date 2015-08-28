f846f73dcca5fd76d3fad968c232016755452cd7
mma219 <mma219@lehigh.edu>
2015-08-28 18:46:06 +0000

    new version

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//Matthew Addessi
+//Demo 0
+
+//Hello World Java Program
+//
+//  first compule the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around it
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("#                       #");
+    System.out.println("#   Hello World 2015!!  #");
+    System.out.println("#                       #");
+    System.out.println("#########################");
+    System.out.println("#########################");
+    System.out.println("test");
+
+    //test/testtest
+
+
+    }
+}
