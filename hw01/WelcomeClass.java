5a110c929a707b6d3f86bf89c8318e2aaf8cd288
mma219 <mma219@lehigh.edu>
2015-08-31 20:41:18 +0000

    push

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+//Matthew Addessi
+//HW 1
+//Professor Chen
+//August 31,2015
+
+//Welcome Class Homework Assignment
+//
+
+//  define the class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statements so that they arrange accordingly
+
+    System.out.println("  ----------- ");
+    System.out.println("  | WELCOME | ");
+    System.out.println("  ----------- ");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-M--M--A--2--1--9-> ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This summer I spent three months working in the research department of an investment firm.");
+    System.out.println("I learned PHP, mySQL, and HTML.");
+
+    }
+}
